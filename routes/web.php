a8a6ecbec1aae9357722f5cd143da443a2aa743a
Tayoo71 <93597450+Tayoo71@users.noreply.github.com>
2024-08-12 12:20:09 +0800

    Update Base Website
    Update base Layout Website

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,47 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home', [
+        'title' => 'Halaman Utama'
+    ]);
+});
+Route::get('/daftarbarang', function () {
+    return view('daftarbarang', [
+        'title' => 'Daftar Barang',
+    ]);
+});
+Route::get('/daftargudang', function () {
+    return view('daftargudang', [
+        'title' => 'Daftar Gudang',
+    ]);
+});
+Route::get('/kartustok', function () {
+    return view('kartustok', [
+        'title' => 'Kartu Stok',
+    ]);
+});
+Route::get('/barangmasuk', function () {
+    return view('barangmasuk', [
+        'title' => 'Barang Masuk',
+    ]);
+});
+Route::get('/barangkeluar', function () {
+    return view('barangkeluar', [
+        'title' => 'Barang Keluar',
+    ]);
+});
+Route::get('/stokopname', function () {
+    return view('stokopname', [
+        'title' => 'Stok Opname',
+    ]);
+});
+Route::get('/itemtransfer', function () {
+    return view('itemtransfer', [
+        'title' => 'Item Transfer',
+    ]);
+});
+Route::get('/laporan', function () {
+    return view('laporan', [
+        'title' => 'Laporan Daftar Barang',
+    ]);
 });
