a38cb203ee79f21edd716f9936454a4a28b96c4f
Tayoo71 <93597450+Tayoo71@users.noreply.github.com>
2024-10-26 23:48:45 +0700

    Simplify Code

---
+++ resources/views/components/modal/format-export-selection.blade.php
@@ -0,0 +1,14 @@
+<div class="col-span-2">
+    <label for="format" class="block text-sm font-medium text-gray-900 dark:text-white">Format
+        Data</label>
+    <p class="mb-2 text-xs text-red-600 dark:text-red-400">
+        Data yang akan dikonversi berdasarkan filter cari yang digunakan saat ini.
+    </p>
+    <select name="format" id="format" required
+        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white">
+        <option value="">Pilih Format Data</option>
+        <option value="pdf">PDF</option>
+        <option value="xlsx">XLSX (Excel)</option>
+        <option value="csv">CSV</option>
+    </select>
+</div>
